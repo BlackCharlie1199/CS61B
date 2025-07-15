1f22e3044c093268b4a94af8daae89d907dd86fa
Charlie <hcc941231@gmail.com>
2025-07-15 15:40:19 +0800

    Change the gravity constant as static final

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,5 +1,5 @@
 public class Planet {
-    public static double constant_G = 6.67e-11;
+    public static final double constant_G = 6.67e-11;
     public double xxPos;
     public double yyPos;
     public double xxVel;
