3c857e64709b45846bcbe84ca3f1f4ffeaa24fd7
Charlie <hcc941231@gmail.com>
2025-07-22 13:09:35 +0800

    remove the assert function from the for loop

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -10,47 +10,28 @@ public class TestArrayDequeGold {
         ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
         StringBuilder errorMessage = new StringBuilder();
 
-        for(int i = 0; i < 1000; i++) {
-            double chance = StdRandom.uniform(0.0, 1.0);
+        for (int i = 0; i < 1000; i++) {
+            student.addFirst(i);
+            solution.addFirst(i);
+            errorMessage.append("addFirst()\n");
+        }
+
+        Integer expect, actual;
+        for(int i = 0; i < 998; i++) {
+            double chance = StdRandom.uniform();
             if (chance < 0.5) {
-                student.addLast(5);
-                solution.addLast(5);
-                errorMessage.append("addLast()\n");
-                Integer expect = solution.removeFirst();
-                Integer actual = student.removeFirst();
+                solution.removeLast();
+                student.removeLast();
                 errorMessage.append("removeLast()\n");
-                assertEquals(errorMessage.toString(), expect, actual);
-                student.addFirst(4);
-                solution.addFirst(4);
-                errorMessage.append("addFirst()\n");
             } else {
-                student.addFirst(1);
-                solution.addFirst(1);
-                errorMessage.append("addFirst()\n");
-                Integer expect = solution.removeLast();
-                Integer actual = student.removeLast();
-                errorMessage.append("removeLast()\n");
-                assertEquals(errorMessage.toString(), expect, actual);
-            }
-        }
-
-        /*
-        for(int i = 0; i < solution.size(); i++) {
-            boolean chance = StdRandom.bernoulli();
-
-            if (chance) {
-                Integer expect = solution.removeFirst();
-                Integer actual = student.removeFirst();
+                solution.removeFirst();
+                student.removeFirst();
                 errorMessage.append("removeFirst()\n");
-                assertEquals(errorMessage.toString(), expect, actual);
-            } else {
-                Integer expect = solution.removeLast();
-                Integer actual = student.removeLast();
-                errorMessage.append("removeLast()\n");
-                assertEquals(errorMessage.toString(), expect, actual);
             }
         }
-
-         */
+        expect = solution.removeLast();
+        actual = student.removeLast();
+        assertEquals(errorMessage.toString(), expect, actual);
     }
+
 }
